20f1ec760fc17b8dcfd42382d6f2b2fc4aa29f9a
Martin Wiso <martin@wiso.cz>
2015-02-19 14:15:04 +0100

    Enable sending messages with custom username and icon

--- src/slacker_chat.erl
+++ src/slacker_chat.erl
@@ -2,7 +2,8 @@
 
 -include("spec.hrl").
 
--export([update/4, delete/3, post_message/3]).
+-export([update/4, delete/3]).
+-export([post_message/3, post_message/4, post_message/5]).
 
 %% @doc Updates a message in a channel.
 -spec update(Token :: string(), TimeStamp :: string(), Channel :: string(), Text :: string()) -> http_response().
@@ -18,3 +19,14 @@ delete(Token, TimeStamp, Channel) ->
 -spec post_message(Token :: string(), Channel :: string(), Message :: string()) -> http_response().
 post_message(Token, Channel, Message) ->
     slacker_request:send("chat.postMessage", [{"token", Token},{"channel", Channel},{"text", Message}]).
+
+%% @doc Post a message as a specific user.
+-spec post_message(Token :: string(), Channel :: string(), Message :: string(), Usrname :: string()) -> http_response().
+post_message(Token, Channel, Message, Username) ->
+    slacker_request:send("chat.postMessage", [{"token", Token},{"channel", Channel}, {"username", Username},{"text", Message}]).
+
+
+%% @doc Post a message as a specific user and custom icon.
+-spec post_message(Token :: string(), Channel :: string(), Message :: string(), Username :: string(), IconUrl :: string()) -> http_response().
+post_message(Token, Channel, Message, Username, IconUrl) ->
+    slacker_request:send("chat.postMessage", [{"token", Token},{"channel", Channel}, {"username", Username}, {"icon_url", IconUrl},{"text", Message}]).
