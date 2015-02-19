6aa022450d291af657e2b2960eb8555f8f3b1847
Martin Wiso <martin@wiso.cz>
2015-02-19 18:19:02 +0100

    Support attachmanets in chat

--- src/slacker_chat.erl
+++ src/slacker_chat.erl
@@ -3,7 +3,7 @@
 -include("spec.hrl").
 
 -export([update/4, delete/3]).
--export([post_message/3, post_message/4, post_message/5]).
+-export([post_message/3, post_message/4, post_message/5, post_message/6]).
 
 %% @doc Updates a message in a channel.
 -spec update(Token :: string(), TimeStamp :: string(), Channel :: string(), Text :: string()) -> http_response().
@@ -30,3 +30,10 @@ post_message(Token, Channel, Message, Username) ->
 -spec post_message(Token :: string(), Channel :: string(), Message :: string(), Username :: string(), IconUrl :: string()) -> http_response().
 post_message(Token, Channel, Message, Username, IconUrl) ->
     slacker_request:send("chat.postMessage", [{"token", Token},{"channel", Channel}, {"username", Username}, {"icon_url", IconUrl},{"text", Message}]).
+
+
+%% @doc Post a message as a specific user and custom icon and attachment.
+-spec post_message(Token :: string(), Channel :: string(), Message :: string(), Username :: string(), IconUrl :: string(), Attachment :: any()) -> http_response().
+post_message(Token, Channel, Message, Username, IconUrl, Attachment) ->
+    slacker_request:send("chat.postMessage", [{"token", Token},{"channel", Channel},
+                                              {"username", Username}, {"icon_url", IconUrl},{"text", Message}, {"attachment", [Attachment]}]).
