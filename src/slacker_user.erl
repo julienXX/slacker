95cf952a73116672e3bce2fdf9e7b3f12a322fb4
Julien Blanchard <julien@sideburns.eu>
2015-05-06 20:53:51 +0200

    Update User endpoint

--- src/slacker_user.erl
+++ src/slacker_user.erl
@@ -2,8 +2,13 @@
 
 -include("spec.hrl").
 
--export([list/1, info/2, active/1]).
+-export([get_presence/2, info/2, list/1, set_active/1, set_presence/2]).
 
+
+%% @doc Gets user presence information.
+-spec get_presence(Token :: string(), User :: string()) -> http_response().
+get_presence(Token, User) ->
+    slacker_request:send("users.getPresence", [{"token", Token},{"user", User}]).
 
 %% @doc List all users in the team.
 -spec list(Token :: string()) -> http_response().
@@ -16,6 +21,11 @@ info(Token, User) ->
     slacker_request:send("users.info", [{"token", Token},{"user", User}]).
 
 %% @doc Marks a user as active.
--spec active(Token :: string()) -> http_response().
-active(Token) ->
+-spec set_active(Token :: string()) -> http_response().
+set_active(Token) ->
     slacker_request:send("users.setActive", [{"token", Token}]).
+
+%% @doc Sets user presence information.
+-spec set_presence(Token :: string(), Presence :: string()) -> http_response().
+set_presence(Token, Presence) ->
+    slacker_request:send("users.setPresence", [{"token", Token},{"presence", Presence}]).
