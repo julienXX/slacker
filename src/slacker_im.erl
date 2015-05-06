e6d261396b4479ece0e82ecf45d692dad883b558
Julien Blanchard <julien@sideburns.eu>
2015-05-06 20:50:12 +0200

    Update IM endpoint

--- src/slacker_im.erl
+++ src/slacker_im.erl
@@ -2,8 +2,13 @@
 
 -include("spec.hrl").
 
--export([history/2, list/1, mark/3]).
+-export([close/2, history/2, list/1, mark/3, open/2]).
 
+
+%% @doc Close a direct message channel.
+-spec close(Token :: string(), Channel :: string()) -> http_response().
+close(Token, Channel) ->
+    slacker_request:send("im.close", [{"token", Token},{"channel", Channel}]).
 
 %% @doc Fetch history of messages and events from a given direct message channel.
 -spec history(Token :: string(), Channel :: string()) -> http_response().
@@ -19,3 +24,8 @@ list(Token) ->
 -spec mark(Token :: string(), Channel :: string(), Timestamp :: string()) -> http_response().
 mark(Token, Channel, Timestamp) ->
     slacker_request:send("im.history", [{"token", Token},{"channel", Channel},{"ts", Timestamp}]).
+
+%% @doc Opens a direct message channel.
+-spec open(Token :: string(), Channel :: string()) -> http_response().
+open(Token, Channel) ->
+    slacker_request:send("im.open", [{"token", Token},{"channel", Channel}]).
