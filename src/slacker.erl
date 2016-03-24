b92ff9a711e56377a8408bba6e5c952113ebb955
ipinak <giannis.pin@gmail.com>
2016-03-24 16:50:23 +0100

    Added missing applications when starting this one

--- src/slacker.erl
+++ src/slacker.erl
@@ -9,7 +9,7 @@
 
 -export([start/0, stop/0]).
 
--define(DEPS, [crypto, asn1, public_key, ssl, inets, slacker]).
+-define(DEPS, [crypto, asn1, public_key, ssl, inets, idna, hackney, restc, slacker]).
 
 -spec start() -> ok.
 start() ->
